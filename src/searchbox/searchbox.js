8b2c86b7053a28b3dcfab68ce6f69febf3f6c8e2
failtide kwan <newman@BMW-bluetooth.alphafoxtrot.one>
2020-06-10 10:36:58 -0700

    added baseic apollo elements

--- src/searchbox/searchbox.js
+++ src/searchbox/searchbox.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './searchbox.css';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const paperQuery = gql`
+  {
+    papers {
+      title
+      id
+      similar {
+        title
+        id
+      }
+    }
+  }
+`
 
 class Searchbox extends React.Component {
 
+
   constructor(props) {
     super(props);
-    this.state = {searchValue: ''};
+    this.state = {searchValue: '', searchResults: null};
 
     this.handleSearchChange = this.handleSearchChange.bind(this);
     this.submitQuery = this.submitQuery.bind(this);
@@ -16,13 +32,31 @@ class Searchbox extends React.Component {
     this.setState({searchValue: event.target.value})
   }
 
+  displayResults(){
+    if(this.state.searchResults){
+    return this.state.searchResults.map(paper => {
+      return(<div key={paper.id}>{paper.title}</div>)
+    })
+    }
+  return (<p>results will show here...</p>)
+  }
+
   submitQuery(event) {
+
     console.log(this.state.searchValue);
     event.preventDefault();
+    let data = this.props.data;
+    if(data.loading){
+      return (<div>Loading Papers</div>)
+    }else{
+      this.setState({searchResults: data.papers})
+    }
+
   }
 
 
   render(){
+
     return(
       <div className="searchbox-container">
         <div className="searchbox">
@@ -35,10 +69,16 @@ class Searchbox extends React.Component {
               <input type="text" className="searchbox-input" value={this.state.searchValue} onChange={this.handleSearchChange} name="paper" />
             </div>
             <div className="input-container">
-              <button className="button button-search" type="submit">find similar papers</button>
+              <button className="button button-search" type="submit" onClick={() => console.log('searching')}>find similar papers</button>
             </div>
             </form>
+
           </div>
+
+          <div id="papers">
+            {this.displayResults()}
+          </div>
+
         </div>
       </div>
     )
@@ -48,4 +88,4 @@ class Searchbox extends React.Component {
 
 
 
-export default Searchbox
+export default     graphql(paperQuery)(Searchbox);
